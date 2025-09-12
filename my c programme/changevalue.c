bb3e6a085f2ea41215eb83e9190db5a7630af5e2
Your Name <you@example.com>
2025-09-12 17:38:11 +0530

    First commit

---
+++ my c programme/changevalue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+int Ram=10;
+
+printf("Ram: %d\n",Ram) ;
+
+Ram=20;
+printf("Ram: %d\n",Ram) ;
+
+ int Shyam=40;
+printf("Shyam: %d",Shyam) ;
+
+return 0;
+}
+
+// Add Variable together
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int a=10;
+//     int b=20;
+//     int c=30;
+//     int sum=a+b+c;
+//     printf("addition of a+b+c: %d", sum) ;
+//     return 0;
+// }
---
+++ my c programme/myfirstprograme.c
@@ -0,0 +1,30 @@
+// int Variable
+
+// #include <stdio.h>
+
+// int main(){
+//    int vishv = 10;
+//     printf("vishv: %d" ,vishv);
+//     return 0;
+// }
+
+// float variable
+
+// #include <stdio.h>
+
+// int main(){
+//    float vishv = 10.23;
+//     printf("vishv: %f" ,vishv);
+//     return 0;
+// }
+
+
+// Char Variable
+
+#include <stdio.h>
+
+int main(){
+    char a = 'v';
+    printf("a: %c",a);
+    return 0;
+}
