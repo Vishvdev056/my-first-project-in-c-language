a83f5f038f322264917a8e9876b942a68a7be1b7
Your Name <you@example.com>
2025-09-12 17:48:34 +0530

    update message

--- my c programme/changevalue.c
+++ my c programme/changevalue.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
-int Ram=10;
+// int main(){
+// int Ram=10;
 
-printf("Ram: %d\n",Ram) ;
+// printf("Ram: %d\n",Ram) ;
 
-Ram=20;
-printf("Ram: %d\n",Ram) ;
+// Ram=20;
+// printf("Ram: %d\n",Ram) ;
 
- int Shyam=40;
-printf("Shyam: %d",Shyam) ;
+//  int Shyam=40;
+// printf("Shyam: %d",Shyam) ;
 
-return 0;
-}
+// return 0;
+// }
 
 // Add Variable together
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     int a=10;
-//     int b=20;
-//     int c=30;
-//     int sum=a+b+c;
-//     printf("addition of a+b+c: %d", sum) ;
-//     return 0;
-// }
+int main(){
+    int a=10;
+    int b=20;
+    int c=30;
+    int sum=a+b+c;
+    printf("addition of a+b+c: %d", sum) ;
+    return 0;
+}
---
+++ my c programme/tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// int main(){
+// int Ram=10;
+
+// printf("Ram: %d\n",Ram) ;
+
+// Ram=20;
+// printf("Ram: %d\n",Ram) ;
+
+//  int Shyam=40;
+// printf("Shyam: %d",Shyam) ;
+
+// return 0;
+// }
