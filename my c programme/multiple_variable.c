af74f9aba3dff7a9e224b5f5f13f3d7a5bfdc1c3
Your Name <you@example.com>
2025-09-13 10:19:14 +0530

    update message

--- my c programme/multiple_variable.c
+++ my c programme/multiple_variable.c
@@ -1,5 +1,28 @@
+// #include <stdio.h>
+
+// int main(){
+//     int age=18;
+//     int height=150;
+//     float weight=46.8;
+//     char Name[]="Vishvdev";
+
+//     printf("Name: %s\n",Name);
+//     printf("Age: %d\n",age);
+//     printf("Height: %d centimeter\n",height);
+//     printf("Weight:%f kg\n",weight);
+//     return 0;
+// }
+
+// Area of Ractangle
+
 #include <stdio.h>
 
-void main(){
-    int 
+int main(){
+    int length=10;
+    int width=5;
+    int area;
+area=length*width ;
+
+printf("Area of Ractangle: %d",area);
+return 0;
 }
