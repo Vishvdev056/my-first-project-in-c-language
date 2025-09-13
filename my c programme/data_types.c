682e57b56640e30e366971f1eb886000d6511b2c
Your Name <you@example.com>
2025-09-13 12:39:53 +0530

    update message

---
+++ my c programme/data_types.c
@@ -0,0 +1,25 @@
+// The Data type specifies the data size and the type of information the variable will store. 
+
+// the char types
+
+// #include <stdio.h>
+
+// int main(){
+//     char mygrade='A';
+//     printf("My Grade : %c", mygrade);
+//     return 0;
+// }
+
+// 2
+
+#include <stdio.h>
+ 
+int main(){
+    char a=60;
+    char b=80;
+    char c=10;
+    printf("first : %d\n",a);
+    printf("second : %d\n",b);
+    printf("third : %d\n",c);
+    return 0;
+}
